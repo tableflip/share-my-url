08273910f92d049530a2fea990e4777781158334
Bernard Mordan <bernardmordan@gmail.com>
2015-07-02 17:10:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var qs = require('querystring')
+
+module.exports = function (network, url, opts) {
+
+  opts = opts || {}
+
+  if (!linkfor[network]) throw new Error('Unsupported network ' + network)
+  return linkfor[network](url, opts)
+
+}
+
+var linkfor = {
+  facebook: function (url, opts) {
+    var share = {
+      u: url
+    }
+    if (opts.t) share.t = opts.t
+    return 'https://www.facebook.com/sharer/sharer.php?' + qs.stringify(share)
+  },
+  google: function (url, opts) {
+    var share = {
+      url: url
+    }
+    return 'https://plus.google.com/share?' + qs.stringify(share)
+  },
+  pinterest: function (url, opts) {
+    var share = {
+      url: url
+    }
+    if (opts.media) share.media = opts.media
+    if (opts.description) share.description = opts.description
+    return 'http://pinterest.com/pin/create/button/?' + qs.stringify(share)
+  },
+  twitter: function (url, opts) {
+    var share = {
+      source: url
+    }
+    if (opts.text) share.text = opts.text
+    if (opts.via) share.via = opts.via
+    return 'https://twitter.com/intent/tweet?' + qs.stringify(share)
+  }
+}
---
+++ test.js
@@ -0,0 +1,63 @@
+var test = require('tape')
+var share = require('./')
+var urlParse = require('url').parse
+
+test('the function throws', function (t) {
+  t.plan(1)
+  t.throws(function () {share('hoola')}, 'if called with unsupported network')
+  t.end()
+})
+test('facebook link', function (t) {
+  t.plan(3)
+  var myLink = 'https://nodejs.org'
+  var myTitle = 'test title'
+  var url = share('facebook', myLink, {t: myTitle})
+  var urlParsed = urlParse(url, true, true)
+
+  t.equal(urlParsed.query.u, myLink, 'url is added to the facebook url')
+  t.equal(urlParsed.query.t, myTitle, 't (title) is added to the facebook url')
+  t.notEqual(urlParsed.hostname.indexOf('facebook.com'), -1)
+  console.log('facebook', url)
+  t.end()
+})
+test('google link', function (t) {
+  t.plan(2)
+  var myLink = 'https://nodejs.org'
+  var url = share('google', myLink)
+  var urlParsed = urlParse(url, true, true)
+
+  t.equal(urlParsed.query.url, myLink, 'url is added to the google url')
+  t.notEqual(urlParsed.hostname.indexOf('google.com'), -1)
+  console.log('google', url)
+  t.end()
+})
+test('twitter link', function (t) {
+  t.plan(4)
+  var myLink = 'https://nodejs.org'
+  var myTitle = 'test title'
+  var myHandle = 'testTwitter'
+  var url = share('twitter', myLink, {text: myTitle, via: myHandle})
+  var urlParsed = urlParse(url, true, true)
+
+  t.equal(urlParsed.query.source, myLink, 'source is added to the twitter url')
+  t.equal(urlParsed.query.text, myTitle, 'title is added to the twitter url')
+  t.equal(urlParsed.query.via, myHandle, 'testTwitter handle is added to the twitter url')
+  t.notEqual(urlParsed.hostname.indexOf('twitter.com'), -1)
+  console.log('twitter', url)
+  t.end()
+})
+test('pinterest link', function (t) {
+  t.plan(4)
+  var myLink = 'https://nodejs.org'
+  var myDescription = 'test title'
+  var myMedia = 'https://cdn.tutsplus.com/net/uploads/legacy/404_regularExpressions/images/url.jpg'
+  var url = share('pinterest', myLink, {description: myDescription, media: myMedia})
+  var urlParsed = urlParse(url, true, true)
+
+  t.equal(urlParsed.query.url, myLink, 'source is added to the twitter url')
+  t.equal(urlParsed.query.description, myDescription, 'title is added to the twitter url')
+  t.equal(urlParsed.query.media, myMedia, 'handle is added to the twitter url')
+  t.notEqual(urlParsed.hostname.indexOf('pinterest.com'), -1)
+  console.log('pinterest', url)
+  t.end()
+})
